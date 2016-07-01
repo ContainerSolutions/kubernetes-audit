2f2abac434b2afaf275769eacbf4a7da53f098d1
Adrian Mouat <adrian@adrianmouat.com>
2016-07-01 14:52:39 +0100

    Added client using official k8s rest stuff.

--- cmd/audit.go
+++ cmd/audit.go
@@ -3,19 +3,64 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"time"
+
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/client/cache"
+	"k8s.io/kubernetes/pkg/client/restclient"
+	client "k8s.io/kubernetes/pkg/client/unversioned"
+	"k8s.io/kubernetes/pkg/controller/framework"
+	"k8s.io/kubernetes/pkg/fields"
+	"k8s.io/kubernetes/pkg/util/wait"
 )
 
-func main() {
+func eventCreated(obj interface{}) {
+	ev := obj.(*api.Event)
+	fmt.Printf("%v %q\n", ev.LastTimestamp, ev.Message)
+}
 
-	f, err := os.Create("audit.log")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error creating audit log file")
-		os.Exit(1)
+func watchEvents(client *client.Client) cache.Store {
+
+	watchlist := cache.NewListWatchFromClient(client, "events", api.NamespaceAll, fields.Everything())
+	resyncPeriod := 0 * time.Minute
+
+	//Setup an informer to call functions when the watchlist changes
+	store, controller := framework.NewInformer(
+		watchlist,
+		&api.Event{},
+		resyncPeriod,
+		framework.ResourceEventHandlerFuncs{
+			AddFunc: eventCreated,
+		},
+	)
+	//Run the controller as a goroutine
+	go controller.Run(wait.NeverStop)
+	return store
+}
+
+/*
+Hacked together listener using Kubernetes restclient.
+*/
+func restVersion() {
+	config := &restclient.Config{
+		Host:     "http://localhost:8080",
+		Insecure: true,
 	}
-	defer f.Close()
+	//Create a new client to interact with cluster and freak if it doesn't work
+	kubeClient, err := client.New(config)
 
+	if err != nil {
+		log.Fatalln("Client not created sucessfully:", err)
+	}
+
+	//This actually returns a cache, but we don't really need it...
+	watchEvents(kubeClient)
+}
+
+func simpleHTTP() {
 	resp, err := http.Get("http://localhost:8080/api/v1/events?watch=true")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed to get events from API server: %v\n", err)
@@ -35,4 +80,12 @@ func main() {
 		}
 	}
 
+}
+
+func main() {
+	restVersion()
+
+	for {
+		time.Sleep(time.Second)
+	}
 }
